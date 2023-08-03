2fe473608039843bc2f1e0450a41fc15524b0758
dullls <102576821+dullls@users.noreply.github.com>
2023-08-03 22:00:04 +0700

    loginG.php

--- app/Views/layout/loginG.php
+++ app/Views/layout/loginG.php
@@ -1,37 +1,105 @@
 <!-- Outer Row -->
-<center>
-    <hr>
-    <div class="row justify-content-center">
+<html>
 
-        <div class="col-xl-10 col-lg-12 col-md-9">
+<head>
+    <title>Cara Membuat Form Login Sederhana Tapi Berkelas</title>
+</head>
+<style>
+    body {
+        background: #456;
+        font-family: 'Open Sans', sans-serif;
+    }
 
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <!-- Nested Row within Card Body -->
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Silahkan Login</h1>
-                                </div>
-                                <form class="user" method="post" action="<?= base_url('auth/valid_loginG'); ?>">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukan Username..." name="username">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Masukan Password..." name="password">
-                                    </div>
-                                    <button class="btn btn-primary btn-user btn-block" type="submit" name="login">Login</button>
-                                </form>
-                                <hr>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    .login {
+        width: 400px;
+        margin: 16px auto;
+        font-size: 16px;
+    }
 
-        </div>
+    /* Reset top and bottom margins from certain elements */
+    .login-header,
+    .login p {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
 
+    .login-header {
+        background: #28d;
+        padding: 20px;
+        font-size: 1.4em;
+        font-weight: normal;
+        text-align: center;
+        text-transform: uppercase;
+        color: #fff;
+    }
+
+    .login-container {
+        background: #ebebeb;
+        padding: 12px;
+    }
+
+    /* Every row inside .login-container is defined with p tags */
+    .login p {
+        padding: 12px;
+    }
+
+    .login input {
+        box-sizing: border-box;
+        display: block;
+        width: 100%;
+        border-width: 1px;
+        border-style: solid;
+        padding: 16px;
+        outline: 0;
+        font-family: inherit;
+        font-size: 0.95em;
+    }
+
+    .login input[type="email"],
+    .login input[type="password"] {
+        background: #fff;
+        border-color: #bbb;
+        color: #555;
+    }
+
+    /* Text fields' focus effect */
+    .login input[type="email"]:focus,
+    .login input[type="password"]:focus {
+        border-color: #888;
+    }
+
+    .login input[type="submit"] {
+        background: #28d;
+        border-color: transparent;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    .login input[type="submit"]:hover {
+        background: #17c;
+    }
+
+    /* Buttons' focus effect */
+    .login input[type="submit"]:focus {
+        border-color: #05a;
+    }
+</style>
+
+<body>
+    <div class="login">
+        <h2 class="login-header">Form Login</h2>
+        <form class="login-container" method="post" action="<?= base_url('auth/valid_loginG'); ?>">
+            <p>
+                <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukan username..." name="username">
+            </p>
+            <p>
+                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Masukan Password..." name="password">
+            </p>
+            <p>
+                <input type="submit" value="Log in">
+            </p>
+        </form>
     </div>
-</center>
+</body>
+
+</html>
